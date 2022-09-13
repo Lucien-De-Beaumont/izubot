45b829d6e479e0636005bb7f69d70931ecada2e6
Lucien-De-Beaumont <raphael50110@yahoo.fr>
2022-09-13 21:35:38 +0200

    [2.9.2] - fixed button perms bug

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -18,16 +18,8 @@ module.exports = {
     if (interaction.isButton()) {
       const btn = client.buttons.get(interaction.customId);
       if (!btn) return interaction.reply("Ce bouton n'existe pas!");
-      let roles = btn.roles;
-      if (btn) {
-        roles = btn.role;
-        if (interaction.member.roles.cache.some(r => roles.includes(r.id)) || interaction.member.id == '553231950958035004') {
-          btn.runInteraction(client, interaction);
-        } else {
-          return interaction.reply("Vous n'avez pas les permissions nécessaires.");
-        }
-      }
-      }
+      btn.runInteraction(client, interaction);
+    }
     if (interaction.isSelectMenu()) {
       const selectMenu = client.selects.get(interaction.customId);
       if (!selectMenu) return interaction.reply("Ce select menu n'existe pas!");
