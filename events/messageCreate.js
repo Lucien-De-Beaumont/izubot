1ff3a5bfe57efea91c46a608c32c045d8c570782
Lucien-De-Beaumont <raphael50110@yahoo.fr>
2022-11-03 17:10:11 +0100

    [API BACKUP - TURN ON messageCreate.js]

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -14,63 +14,63 @@ module.exports = {
     let webhookName
     let prefix
 
-    // const [results] = await db.query(`SELECT * FROM HeroicAcademy WHERE discordid=${db.escape(message.author.id)} ORDER BY CHAR_LENGTH(prefix) DESC`)
+    const [results] = await db.query(`SELECT * FROM HeroicAcademy WHERE discordid=${db.escape(message.author.id)} ORDER BY CHAR_LENGTH(prefix) DESC`)
 
-    // for (element of results) {
-    //   allResultsForDate.push(element.date)
-    // }
+    for (element of results) {
+      allResultsForDate.push(element.date)
+    }
 
-    // for (index in allResultsForDate) {
-    //   if (message.content.startsWith(results[index].prefix)) {
-    //     prefix = results[index].prefix;
-    //     webhookName = results[index].nom;
-    //     imgURL = results[index].iconURL;
-    //     break;
-    //   }
-    // }
-    // let webhooks = await message.channel.fetchWebhooks()
-    // let webhook = webhooks.find(wh => wh.owner.id == client.user.id)
+    for (index in allResultsForDate) {
+      if (message.content.startsWith(results[index].prefix)) {
+        prefix = results[index].prefix;
+        webhookName = results[index].nom;
+        imgURL = results[index].iconURL;
+        break;
+      }
+    }
+    let webhooks = await message.channel.fetchWebhooks()
+    let webhook = webhooks.find(wh => wh.owner.id == client.user.id)
 
-    // if (message.content.startsWith(prefix)) {
-    //   let args = message.content.slice(prefix.length).trim().split(/ +/g);
-    //   let content = args.slice(0).join(" ").replace(/prefix/i);
-    //   let ping
-    //   if (message.reference !== null) {
-    //     msg = await client.channels.cache.get(message.channel.id).messages.fetch(message.reference.messageId)
-    //     if (!(msg.content.includes('> ') && msg.content.includes('Réponse à') && msg.content.includes('\[Message initial\]\('))) {
-    //       if (msg.author.bot) {
-    //         const [results0] = await db.query(`SELECT * FROM HeroicAcademy WHERE iconURL = '${msg.author.displayAvatarURL()}' OR nom = '${msg.author.username}'`)
-    //         ping = results0[0].discordid
-    //       } else {
-    //         ping = msg.author.id
-    //       }
-    //       content = '> ' + msg.content.replaceAll('\n', '\n> ') + '\n_Réponse à ' + msg.author.username + ' ( <@' + ping + '> )' + '_ —— [Message initial](' + msg.url + ')\n\n' + content
-    //     }
-    //   }
-    //   if (typeof webhook == 'undefined') {
-    //     webhook = await message.channel.createWebhook(`${message.channel.name}`, { avatar: client.user.displayAvatarURL() })
-    //   }
-    //   if (message.attachments.map(i => i)[0] && content.length) {
-    //     await webhook.send({
-    //       content: content,
-    //       files: [...message.attachments.values()],
-    //       username: webhookName,
-    //       avatarURL: imgURL,
-    //     })
-    //   } else if (message.attachments.map(i => i)[0]) {
-    //     await webhook.send({
-    //       files: [...message.attachments.values()],
-    //       username: webhookName,
-    //       avatarURL: imgURL,
-    //     })
-    //   } else {
-    //     await webhook.send({
-    //       content: content,
-    //       username: webhookName,
-    //       avatarURL: imgURL,
-    //     });
-    //   }
-    //   message.delete()
-    // }
+    if (message.content.startsWith(prefix)) {
+      let args = message.content.slice(prefix.length).trim().split(/ +/g);
+      let content = args.slice(0).join(" ").replace(/prefix/i);
+      let ping
+      if (message.reference !== null) {
+        msg = await client.channels.cache.get(message.channel.id).messages.fetch(message.reference.messageId)
+        if (!(msg.content.includes('> ') && msg.content.includes('Réponse à') && msg.content.includes('\[Message initial\]\('))) {
+          if (msg.author.bot) {
+            const [results0] = await db.query(`SELECT * FROM HeroicAcademy WHERE iconURL = '${msg.author.displayAvatarURL()}' OR nom = '${msg.author.username}'`)
+            ping = results0[0].discordid
+          } else {
+            ping = msg.author.id
+          }
+          content = '> ' + msg.content.replaceAll('\n', '\n> ') + '\n_Réponse à ' + msg.author.username + ' ( <@' + ping + '> )' + '_ —— [Message initial](' + msg.url + ')\n\n' + content
+        }
+      }
+      if (typeof webhook == 'undefined') {
+        webhook = await message.channel.createWebhook(`${message.channel.name}`, { avatar: client.user.displayAvatarURL() })
+      }
+      if (message.attachments.map(i => i)[0] && content.length) {
+        await webhook.send({
+          content: content,
+          files: [...message.attachments.values()],
+          username: webhookName,
+          avatarURL: imgURL,
+        })
+      } else if (message.attachments.map(i => i)[0]) {
+        await webhook.send({
+          files: [...message.attachments.values()],
+          username: webhookName,
+          avatarURL: imgURL,
+        })
+      } else {
+        await webhook.send({
+          content: content,
+          username: webhookName,
+          avatarURL: imgURL,
+        });
+      }
+      message.delete()
+    }
   },
 };
